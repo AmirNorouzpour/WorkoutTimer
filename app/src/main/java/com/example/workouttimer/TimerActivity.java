2c31e8a8db36493d7b597eee519845026660e5ee
Amir Norouzpour <Amirnorouzpour@live.com>
2025-01-08 12:33:12 +0100

    Initial commit

---
+++ app/src/main/java/com/example/workouttimer/TimerActivity.java
@@ -0,0 +1,259 @@
+package com.example.workouttimer;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
+import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.media.SoundPool;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TimerActivity extends AppCompatActivity {
+
+    private TextView progressTextTime; // TextView برای نمایش متن زمان و ست
+    private TextView progressTextPhase; // TextView برای نمایش متن زمان و ست
+    private int totalSets; // تعداد کل ست‌ها (برای نمایش شماره ست‌ها)
+    private TextView totalTimerText; // TextView برای تایمر کلی
+    private ImageButton closeBtn;
+    private int totalRemainingTime; // زمان کل باقی‌مانده به ثانیه
+    private Handler totalTimerHandler = new Handler();
+    private Runnable totalTimerRunnable;
+    private SoundPool soundPool;
+    private int sound1, sound2, sound3, sound4, sound5; // شناسه‌های صداها
+    private int lastTimeRead = 1000; // برای جلوگیری از پخش چندباره
+    boolean isFirst = true;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+        setContentView(R.layout.activity_timer);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        soundPool = new SoundPool.Builder()
+                .setMaxStreams(5)
+                .build();
+
+        sound1 = soundPool.load(this, R.raw.number_1, 1);
+        sound2 = soundPool.load(this, R.raw.number_2, 1);
+        sound3 = soundPool.load(this, R.raw.number_3, 1);
+        sound4 = soundPool.load(this, R.raw.number_4, 1);
+        sound5 = soundPool.load(this, R.raw.number_5, 1);
+
+
+        // دسترسی به TextView
+        progressTextPhase = findViewById(R.id.progress_text_phase);
+        progressTextTime = findViewById(R.id.progress_text_time);
+        totalTimerText = findViewById(R.id.total_timer);
+        closeBtn = findViewById(R.id.close_button);
+
+        closeBtn.setOnClickListener(v -> {
+            finish();
+        });
+
+        // دریافت داده‌های Intent
+        totalSets = getIntent().getIntExtra("sets", 2); // تعداد کل ست‌ها
+        int work = getIntent().getIntExtra("work", 30); // مدت زمان work
+        int rest = getIntent().getIntExtra("rest", 15); // مدت زمان rest
+        boolean skip = getIntent().getBooleanExtra("skip", true); // آیا استراحت آخر باید رد شود
+
+
+        int prepareTime = 5; // زمان آماده‌سازی
+        totalRemainingTime = (work + rest) * totalSets + prepareTime - (skip ? rest : 0);
+
+        // شروع تایمر کل
+        startTotalTimer();
+
+        // اجرای انیمیشن‌ها
+        executeSet(totalSets, work, rest, skip);
+    }
+
+    private void executeSet(int sets, int workTime, int restTime, boolean skipLastRest) {
+        if (sets <= 0) return; // اگر تعداد ست‌ها صفر باشد، کار متوقف می‌شود.
+
+        int currentSetNumber = totalSets - sets + 1; // محاسبه شماره ست از 1 شروع شود
+
+        // نمایش Work
+        if (isFirst)
+            updateProgressText(currentSetNumber, "Prepare", -1);
+        executeProgress(isFirst ? 5 : 0, "#FFEB3B", () -> {
+
+            updateProgressText(currentSetNumber, "Work", totalSets);
+            executeProgress(isFirst ? workTime - 1 : workTime, "#ff0054", () -> { // اجرای Work
+                if (sets > 1 || !skipLastRest) { // نمایش Rest
+                    isFirst = false;
+                    updateProgressText(currentSetNumber, "Rest", skipLastRest ? totalSets - 1 : totalSets);
+                    executeProgress(restTime, "#4361ee", () -> {
+                        executeSet(sets - 1, workTime, restTime, skipLastRest); // اجرای ست بعدی
+                    });
+                } else {
+                    this.finish();
+                }
+            });
+        });
+
+    }
+
+    private void executeProgress(int time, String color, Runnable onComplete) {
+        View bottomView = findViewById(R.id.bottom_view);
+        bottomView.setBackgroundColor(Color.parseColor(color));
+
+        int screenHeight = getResources().getDisplayMetrics().heightPixels; // ارتفاع کل صفحه
+
+        ValueAnimator animator = ValueAnimator.ofFloat(0, screenHeight);
+        animator.setDuration(time * 1000L);
+
+        animator.addUpdateListener(animation -> {
+            float animatedValue = (float) animation.getAnimatedValue();
+            ViewGroup.LayoutParams layoutParams = bottomView.getLayoutParams();
+            layoutParams.height = (int) animatedValue;
+            bottomView.setLayoutParams(layoutParams);
+
+            double remainingTime = time - (animation.getCurrentPlayTime() / 1000.0);
+            updateTimeText(Math.max(0, remainingTime));
+
+            if (remainingTime <= 6 && remainingTime > 0 && lastTimeRead > remainingTime) {
+                int remainingInt = (int) Math.ceil(remainingTime);
+                lastTimeRead = remainingInt - 1;
+                playCountdownSound(remainingInt);
+            }
+        });
+
+        animator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                if (onComplete != null) {
+                    onComplete.run();
+                    lastTimeRead = 1000;
+                }
+            }
+        });
+
+        animator.start();
+    }
+
+
+    private void playCountdownSound(int number) {
+        new Thread(() -> {
+            switch (number) {
+                case 1:
+                    soundPool.play(sound1, 1, 1, 0, 0, 1);
+                    break;
+                case 2:
+                    soundPool.play(sound2, 1, 1, 0, 0, 1);
+                    break;
+                case 3:
+                    soundPool.play(sound3, 1, 1, 0, 0, 1);
+                    break;
+                case 4:
+                    soundPool.play(sound4, 1, 1, 0, 0, 1);
+                    break;
+                case 5:
+                    soundPool.play(sound5, 1, 1, 0, 0, 1);
+                    break;
+            }
+        }).start();
+    }
+
+
+    private void updateProgressText(int setNumber, String phase, int totalSets) {
+        String text = phase + "  " + setNumber + "/" + totalSets;
+        if (totalSets == -1)
+            text = phase;
+
+        updateTextWithAnimation(progressTextPhase, text);
+        progressTextPhase.setText(text);
+    }
+
+
+    private void updateTimeText(double remainingTime) {
+        String formattedTime = formatTime((int) Math.ceil(remainingTime));
+        updateTextWithAnimation(progressTextTime, formattedTime);
+        progressTextTime.setText(formattedTime);
+
+    }
+
+
+    private void updateTotalTimerText() {
+        String formattedTime = formatTime(totalRemainingTime);
+        updateTextWithAnimation(totalTimerText, formattedTime);
+        totalTimerText.setText(formattedTime);
+    }
+
+
+    private void startTotalTimer() {
+        totalTimerRunnable = new Runnable() {
+            @Override
+            public void run() {
+                if (totalRemainingTime > 0) {
+                    totalRemainingTime--; // کم کردن یک ثانیه
+                    updateTotalTimerText(); // به‌روزرسانی تایمر کل
+
+                    // اجرای دوباره Runnable بعد از 1 ثانیه
+                    totalTimerHandler.postDelayed(this, 1000);
+                } else {
+                    // وقتی تایمر کل به پایان رسید
+                    totalTimerHandler.removeCallbacks(totalTimerRunnable);
+                }
+            }
+        };
+
+        // شروع اجرای Runnable
+        totalTimerHandler.post(totalTimerRunnable);
+    }
+
+    private void updateTextWithAnimation(TextView textView, String newText) {
+        String string = textView.getText().toString();
+        if (!string.equals(newText)) {
+            textView.setText(newText);
+            animateTextView(textView);
+        }
+    }
+
+    private void animateTextView(TextView textView) {
+        ScaleAnimation scaleAnimation = new ScaleAnimation(
+                1.0f, 1.2f, // بزرگ شدن از ۱ به ۱.۲
+                1.0f, 1.2f, // بزرگ شدن از ۱ به ۱.۲
+                Animation.RELATIVE_TO_SELF, 0.5f, // مرکز افقی
+                Animation.RELATIVE_TO_SELF, 0.5f // مرکز عمودی
+        );
+        scaleAnimation.setDuration(300); // مدت زمان انیمیشن
+        scaleAnimation.setRepeatMode(Animation.REVERSE); // بازگشت به حالت اولیه
+        scaleAnimation.setRepeatCount(1); // انیمیشن یک بار اجرا شود
+        textView.startAnimation(scaleAnimation);
+    }
+
+
+    @SuppressLint("DefaultLocale")
+    private String formatTime(int timeInSeconds) {
+        int minutes = timeInSeconds / 60;
+        int seconds = timeInSeconds % 60;
+        return String.format("%02d:%02d", minutes, seconds);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (totalTimerHandler != null && totalTimerRunnable != null) {
+            totalTimerHandler.removeCallbacks(totalTimerRunnable);
+        }
+
+        if (soundPool != null) {
+            soundPool.release();
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/workouttimer/ui/home/HomeFragment.java
@@ -0,0 +1,174 @@
+package com.example.workouttimer.ui.home;
+
+import static android.content.Context.MODE_PRIVATE;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.workouttimer.R;
+import com.example.workouttimer.TimerActivity;
+import com.example.workouttimer.databinding.FragmentHomeBinding;
+
+public class HomeFragment extends Fragment {
+
+    private FragmentHomeBinding binding;
+    int sets = 2;
+    int workTime = 30;
+    int restTime = 15;
+    boolean skipLastRest = true;
+    TextView timer_text;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+
+        Button buttonIncrement = root.findViewById(R.id.button_sets_increment);
+        Button buttonDecrement = root.findViewById(R.id.button_sets_decrement);
+        TextView textViewValue = root.findViewById(R.id.textview_sets_timer);
+        timer_text = root.findViewById(R.id.timer_text);
+        ConstraintLayout start_workout = root.findViewById(R.id.start);
+
+        start_workout.setOnClickListener(view -> {
+            Intent myIntent = new Intent(getActivity(), TimerActivity.class);
+            myIntent.putExtra("sets", sets);
+            myIntent.putExtra("work", workTime);
+            myIntent.putExtra("rest", restTime);
+            myIntent.putExtra("skip", skipLastRest);
+            startActivity(myIntent);
+
+            SaveData();
+        });
+
+        buttonIncrement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sets++;
+                textViewValue.setText(String.valueOf(sets));
+                SetTotal();
+            }
+        });
+
+        buttonDecrement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (sets > 1) {
+                    sets--; //
+                    textViewValue.setText(String.valueOf(sets));
+                    SetTotal();
+                }
+            }
+        });
+
+
+        // Work Section
+        TextView workTimer = root.findViewById(R.id.textview_work_timer);
+        Button workIncrement = root.findViewById(R.id.button_work_increment);
+        Button workDecrement = root.findViewById(R.id.button_work_decrement);
+
+        workIncrement.setOnClickListener(v -> {
+            workTime += 5;
+            workTimer.setText(formatTime(workTime));
+            SetTotal();
+        });
+
+        workDecrement.setOnClickListener(v -> {
+            if (workTime > 5) {
+                workTime -= 5;
+                workTimer.setText(formatTime(workTime));
+                SetTotal();
+            }
+        });
+
+        // Rest Section
+        TextView restTimer = root.findViewById(R.id.textview_rest_timer);
+        Button restIncrement = root.findViewById(R.id.button_rest_increment);
+        Button restDecrement = root.findViewById(R.id.button_rest_decrement);
+
+        restIncrement.setOnClickListener(v -> {
+            restTime += 5;
+            restTimer.setText(formatTime(restTime));
+            SetTotal();
+        });
+
+        restDecrement.setOnClickListener(v -> {
+            if (restTime > 5) {
+                restTime -= 5;
+                restTimer.setText(formatTime(restTime));
+                SetTotal();
+            }
+        });
+
+        // Skip Last Rest Switch
+        Switch skipLastRest = root.findViewById(R.id.switch_skip_last_rest);
+        skipLastRest.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            this.skipLastRest = isChecked;
+            SetTotal();
+        });
+        LoadData();
+        textViewValue.setText(String.valueOf(sets));
+        workTimer.setText(formatTime(workTime));
+        restTimer.setText(formatTime(restTime));
+        SetTotal();
+        return root;
+    }
+
+    private void SaveData() {
+        SharedPreferences sharedPreferences = requireContext().getSharedPreferences("UserPrefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putInt("sets", sets);
+        editor.putInt("work", workTime);
+        editor.putInt("rest", restTime);
+        editor.putBoolean("skip", skipLastRest);
+        editor.apply();
+    }
+
+    private void LoadData() {
+        SharedPreferences sharedPreferences = requireContext().getSharedPreferences("UserPrefs", MODE_PRIVATE);
+
+        sets = sharedPreferences.getInt("sets", 2);
+        workTime = sharedPreferences.getInt("work", 60);
+        restTime = sharedPreferences.getInt("rest", 20);
+        skipLastRest = sharedPreferences.getBoolean("skip", true);
+
+    }
+
+    @SuppressLint("DefaultLocale")
+    private String formatTime(int seconds) {
+        int minutes = seconds / 60;
+        int secs = seconds % 60;
+        return String.format("%02d:%02d", minutes, secs);
+    }
+
+    private void SetTotal() {
+        int works = sets * workTime;
+        int rests = sets * restTime;
+        int total = works + rests;
+        if (skipLastRest)
+            total = total - restTime;
+        timer_text.setText(formatTime(total));
+    }
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
